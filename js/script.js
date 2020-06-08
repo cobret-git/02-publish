3c32fbd4bcbabeb74755c9829529461744ce1666
Kostya Britikov <konstantin.britikov@gmail.com>
2020-06-08 16:41:47 +0300

    Подготовка к адаптиву и переработка обоих слайдеров
    - Перевод единиц некоторых значений c px в em (остальные позже);
    - Переработка слайдеров;
    - Подготовка к адаптиву всей страницы;

--- js/script.js
+++ js/script.js
@@ -9,9 +9,7 @@ $(function(){
 
 
 $('.slider-depiction').slick({
-    // arrows: false
     lazyLoad: 'ondemand',
-    swipe: false,
     prevArrow: $('.slider-depiction__arrow-prev'),
     nextArrow: $('.slider-depiction__arrow-next'),
     asNavFor: '.slider-description'
@@ -22,19 +20,4 @@ $('.slider-description').slick({
     fade: true,
     asNavFor: '.slider-depiction'
 })
-// $('.portfolio__bottom-grid').isotope({
-
-// });
-// $('.portfolio__bottom-button').on('click touchart', function(){
-//     $('.portfolio__bottom-button').removeClass('portfolio__bottom-button--active');
-//     $(this).addClass('portfolio__bottom-button--active');
-//     var filterValue = $(this).attr('data-i');
-//     console.log(filterValue)
-//     $('.portfolio__bottom-grid').isotope({ filter: filterValue})
-// })
-// $('.portfolio__bottom-grid').isotope({
-//     getSortData: {
-
-//     }
-// })
 });
